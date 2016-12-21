5aea8a4010d4025a6615ac0dc65a38ef8a55896c
Maris <maris@pycom.io>
2016-12-21 10:07:54 +0100

    Examples: Add lopy-to-lopy example.

---
+++ examples/lopy-lopy/lopy-A/main.py
@@ -0,0 +1,12 @@
+from network import LoRa
+import socket
+import time
+
+lora = LoRa(mode=LoRa.LORA, frequency=863000000)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+
+while True:
+    if s.recv(64) == b'Ping':
+        s.send('Pong')
+    time.sleep(5)
---
+++ examples/lopy-lopy/lopy-B/main.py
@@ -0,0 +1,11 @@
+from network import LoRa
+import socket
+import time
+
+lora = LoRa(mode=LoRa.LORA, frequency=863000000)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+
+while True:
+    s.send('Ping')
+    time.sleep(5)
