8656e0e8a7df28e93bb1370ea8e5467e47cc0627
Alex Bucknall <alex.bucknall@gmail.com>
2017-02-13 12:15:31 +0000

    Added Sigfox Uplink example

---
+++ examples/sigfoxUplink/main.py
@@ -0,0 +1,17 @@
+from network import Sigfox
+import socket
+
+# init Sigfox for RCZ1 (Europe)
+sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
+
+# create a Sigfox socket
+s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
+
+# make the socket blocking
+s.setblocking(True)
+
+# configure it as uplink only
+s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
+
+# send some bytes 
+s.send(bytes([0x01, 0x02, 0x03])
