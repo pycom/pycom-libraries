5a47df9532dc1e2ec60eadcae9a3554f3ecc8529
Alex Bucknall <alex@pycom.io>
2017-06-12 15:55:26 +0100

    Pysense & Pytrack changed formatting for convention

--- pysense/lib/LTR329ALS01.py
+++ pysense/lib/LTR329ALS01.py
@@ -2,58 +2,58 @@ import time
 
 class LTR329ALS01:
 
-	ALS_I2CADDR = const(0x29)
-	ALS_CONTR_REG = const(0x80)
-	ALS_MEAS_REG = const(0x85)
-	ALS_DATA_CH0_LOW = const(0x8A)
-	ALS_DATA_CH0_HIGH = const(0x8B)
-	ALS_DATA_CH1_LOW = const(0x88)
-	ALS_DATA_CH1_HIGH = const(0x89)
-	ALS_GAIN_1X = const(0x01)
-	ALS_GAIN_2X = const(0x05)
-	ALS_GAIN_4X = const(0x09)
-	ALS_GAIN_8X = const(0x0D)
-	ALS_GAIN_48X = const(0x19)
-	ALS_GAIN_96X = const(0x1D)
-	ALS_INT_50 = const(0x01)
-	ALS_INT_100 = const(0x00)
-	ALS_INT_150 = const(0x04)
-	ALS_INT_200 = const(0x02)
-	ALS_INT_250 = const(0x05)
-	ALS_INT_300 = const(0x06)
-	ALS_INT_350 = const(0x07)
-	ALS_INT_400 = const(0x03)
-	ALS_RATE_50 = const(0x00)
-	ALS_RATE_100 = const(0x01)
-	ALS_RATE_200 = const(0x02)
-	ALS_RATE_500 = const(0x03)
-	ALS_RATE_1000 = const(0x04)
-	ALS_RATE_2000 = const(0x05)
+    ALS_I2CADDR = const(0x29)
+    ALS_CONTR_REG = const(0x80)
+    ALS_MEAS_REG = const(0x85)
+    ALS_DATA_CH0_LOW = const(0x8A)
+    ALS_DATA_CH0_HIGH = const(0x8B)
+    ALS_DATA_CH1_LOW = const(0x88)
+    ALS_DATA_CH1_HIGH = const(0x89)
+    ALS_GAIN_1X = const(0x01)
+    ALS_GAIN_2X = const(0x05)
+    ALS_GAIN_4X = const(0x09)
+    ALS_GAIN_8X = const(0x0D)
+    ALS_GAIN_48X = const(0x19)
+    ALS_GAIN_96X = const(0x1D)
+    ALS_INT_50 = const(0x01)
+    ALS_INT_100 = const(0x00)
+    ALS_INT_150 = const(0x04)
+    ALS_INT_200 = const(0x02)
+    ALS_INT_250 = const(0x05)
+    ALS_INT_300 = const(0x06)
+    ALS_INT_350 = const(0x07)
+    ALS_INT_400 = const(0x03)
+    ALS_RATE_50 = const(0x00)
+    ALS_RATE_100 = const(0x01)
+    ALS_RATE_200 = const(0x02)
+    ALS_RATE_500 = const(0x03)
+    ALS_RATE_1000 = const(0x04)
+    ALS_RATE_2000 = const(0x05)
 
-	def __init__(self, pysense=None, sda='P22', scl='P21', gain=ALS_GAIN_1X, integration=ALS_INT_100, rate=ALS_RATE_500):
-		if pysense is not None:
-			self.i2c = pysense.i2c
-		else:
-			from machine import I2C
-			self.i2c = I2C(0, mode=I2C.MASTER, pins=(sda, scl))
+    def __init__(self, pysense=None, sda='P22', scl='P21', gain=ALS_GAIN_1X, integration=ALS_INT_100, rate=ALS_RATE_500):
+        if pysense is not None:
+            self.i2c = pysense.i2c
+        else:
+            from machine import I2C
+            self.i2c = I2C(0, mode=I2C.MASTER, pins=(sda, scl))
 
-		self.i2c.writeto_mem(ALS_I2CADDR, ALS_CONTR_REG, bytearray([gain]))
-		meas = self._concat_hex(integration, rate)
-		self.i2c.writeto_mem(ALS_I2CADDR, ALS_MEAS_REG, bytearray([meas]))
-		time.sleep(0.01)
+        self.i2c.writeto_mem(ALS_I2CADDR, ALS_CONTR_REG, bytearray([gain]))
+        meas = self._concat_hex(integration, rate)
+        self.i2c.writeto_mem(ALS_I2CADDR, ALS_MEAS_REG, bytearray([meas]))
+        time.sleep(0.01)
 
-	def _concat_hex(self, a, b):
-		sizeof_b = 0
-		while((b >> sizeof_b) > 0):
-			sizeof_b += 1
-		sizeof_b += sizeof_b % 4
-		return (a << sizeof_b) | b
+    def _concat_hex(self, a, b):
+        sizeof_b = 0
+        while((b >> sizeof_b) > 0):
+            sizeof_b += 1
+        sizeof_b += sizeof_b % 4
+        return (a << sizeof_b) | b
 
-	def lux(self):
-		data0 = self.i2c.readfrom_mem(ALS_I2CADDR , ALS_DATA_CH1_LOW, 1)
-		data1 = self.i2c.readfrom_mem(ALS_I2CADDR , ALS_DATA_CH1_HIGH, 1)
-		data2 = self.i2c.readfrom_mem(ALS_I2CADDR , ALS_DATA_CH0_LOW, 1)
-		data3 = self.i2c.readfrom_mem(ALS_I2CADDR , ALS_DATA_CH0_HIGH, 1)
-		data_reg_CH1 = self._concat_hex(data1[0], data0[0])
-		data_reg_CH0 = self._concat_hex(data2[0], data3[0])
-		return(data_reg_CH0, data_reg_CH1)
+    def lux(self):
+        data0 = self.i2c.readfrom_mem(ALS_I2CADDR , ALS_DATA_CH1_LOW, 1)
+        data1 = self.i2c.readfrom_mem(ALS_I2CADDR , ALS_DATA_CH1_HIGH, 1)
+        data2 = self.i2c.readfrom_mem(ALS_I2CADDR , ALS_DATA_CH0_LOW, 1)
+        data3 = self.i2c.readfrom_mem(ALS_I2CADDR , ALS_DATA_CH0_HIGH, 1)
+        data_reg_CH1 = self._concat_hex(data1[0], data0[0])
+        data_reg_CH0 = self._concat_hex(data2[0], data3[0])
+        return(data_reg_CH0, data_reg_CH1)
--- pysense/lib/MPL3115A2.py
+++ pysense/lib/MPL3115A2.py
@@ -1,113 +1,113 @@
 import time
 
 class MPL3115A2exception(Exception):
-	pass
+    pass
 
 class MPL3115A2:
 
-	MPL3115_I2CADDR             = const(0x60)
-	MPL3115_STATUS              = const(0x00)
-	MPL3115_PRESSURE_DATA_MSB   = const(0x01)
-	MPL3115_PRESSURE_DATA_CSB   = const(0x02)
-	MPL3115_PRESSURE_DATA_LSB   = const(0x03)
-	MPL3115_TEMP_DATA_MSB       = const(0x04)
-	MPL3115_TEMP_DATA_LSB       = const(0x05)
-	MPL3115_DR_STATUS           = const(0x06)
-	MPL3115_DELTA_DATA          = const(0x07)
-	MPL3115_WHO_AM_I            = const(0x0c)
-	MPL3115_FIFO_STATUS         = const(0x0d)
-	MPL3115_FIFO_DATA           = const(0x0e)
-	MPL3115_FIFO_SETUP          = const(0x0e)
-	MPL3115_TIME_DELAY          = const(0x10)
-	MPL3115_SYS_MODE            = const(0x11)
-	MPL3115_INT_SORCE           = const(0x12)
-	MPL3115_PT_DATA_CFG         = const(0x13)
-	MPL3115_BAR_IN_MSB          = const(0x14)
-	MPL3115_P_ARLARM_MSB        = const(0x16)
-	MPL3115_T_ARLARM            = const(0x18)
-	MPL3115_P_ARLARM_WND_MSB    = const(0x19)
-	MPL3115_T_ARLARM_WND        = const(0x1b)
-	MPL3115_P_MIN_DATA          = const(0x1c)
-	MPL3115_T_MIN_DATA          = const(0x1f)
-	MPL3115_P_MAX_DATA          = const(0x21)
-	MPL3115_T_MAX_DATA          = const(0x24)
-	MPL3115_CTRL_REG1           = const(0x26)
-	MPL3115_CTRL_REG2           = const(0x27)
-	MPL3115_CTRL_REG3           = const(0x28)
-	MPL3115_CTRL_REG4           = const(0x29)
-	MPL3115_CTRL_REG5           = const(0x2a)
-	MPL3115_OFFSET_P            = const(0x2b)
-	MPL3115_OFFSET_T            = const(0x2c)
-	MPL3115_OFFSET_H            = const(0x2d)
-	ALTITUDE		            = const(0)
-	PRESSURE		            = const(1)
+    MPL3115_I2CADDR             = const(0x60)
+    MPL3115_STATUS              = const(0x00)
+    MPL3115_PRESSURE_DATA_MSB   = const(0x01)
+    MPL3115_PRESSURE_DATA_CSB   = const(0x02)
+    MPL3115_PRESSURE_DATA_LSB   = const(0x03)
+    MPL3115_TEMP_DATA_MSB       = const(0x04)
+    MPL3115_TEMP_DATA_LSB       = const(0x05)
+    MPL3115_DR_STATUS           = const(0x06)
+    MPL3115_DELTA_DATA          = const(0x07)
+    MPL3115_WHO_AM_I            = const(0x0c)
+    MPL3115_FIFO_STATUS         = const(0x0d)
+    MPL3115_FIFO_DATA           = const(0x0e)
+    MPL3115_FIFO_SETUP          = const(0x0e)
+    MPL3115_TIME_DELAY          = const(0x10)
+    MPL3115_SYS_MODE            = const(0x11)
+    MPL3115_INT_SORCE           = const(0x12)
+    MPL3115_PT_DATA_CFG         = const(0x13)
+    MPL3115_BAR_IN_MSB          = const(0x14)
+    MPL3115_P_ARLARM_MSB        = const(0x16)
+    MPL3115_T_ARLARM            = const(0x18)
+    MPL3115_P_ARLARM_WND_MSB    = const(0x19)
+    MPL3115_T_ARLARM_WND        = const(0x1b)
+    MPL3115_P_MIN_DATA          = const(0x1c)
+    MPL3115_T_MIN_DATA          = const(0x1f)
+    MPL3115_P_MAX_DATA          = const(0x21)
+    MPL3115_T_MAX_DATA          = const(0x24)
+    MPL3115_CTRL_REG1           = const(0x26)
+    MPL3115_CTRL_REG2           = const(0x27)
+    MPL3115_CTRL_REG3           = const(0x28)
+    MPL3115_CTRL_REG4           = const(0x29)
+    MPL3115_CTRL_REG5           = const(0x2a)
+    MPL3115_OFFSET_P            = const(0x2b)
+    MPL3115_OFFSET_T            = const(0x2c)
+    MPL3115_OFFSET_H            = const(0x2d)
+    ALTITUDE		            = const(0)
+    PRESSURE		            = const(1)
 
-	def __init__(self, pysense=None, sda='P22', scl='P21'):
-		if pysense is not None:
-			self.i2c = pysense.i2c
-		else:
-			from machine import I2C
-			self.i2c = I2C(0, mode=I2C.MASTER, pins=(sda, scl))
+    def __init__(self, pysense=None, sda='P22', scl='P21'):
+        if pysense is not None:
+            self.i2c = pysense.i2c
+        else:
+            from machine import I2C
+            self.i2c = I2C(0, mode=I2C.MASTER, pins=(sda, scl))
 
-		self.STA_reg = bytearray(1)
+        self.STA_reg = bytearray(1)
 
-		self.i2c.writeto_mem(MPL3115_I2CADDR, MPL3115_CTRL_REG1, bytes([0xB8]))
-		self.i2c.writeto_mem(MPL3115_I2CADDR, MPL3115_PT_DATA_CFG, bytes([0x07]))
-		self.i2c.writeto_mem(MPL3115_I2CADDR, MPL3115_CTRL_REG1, bytes([0xB9]))
+        self.i2c.writeto_mem(MPL3115_I2CADDR, MPL3115_CTRL_REG1, bytes([0xB8]))
+        self.i2c.writeto_mem(MPL3115_I2CADDR, MPL3115_PT_DATA_CFG, bytes([0x07]))
+        self.i2c.writeto_mem(MPL3115_I2CADDR, MPL3115_CTRL_REG1, bytes([0xB9]))
 
-		if self._read_status():
-			pass
-		else:
-			raise MPL3115A2exception("Error with MPL3115A2")
+        if self._read_status():
+            pass
+        else:
+            raise MPL3115A2exception("Error with MPL3115A2")
 
-	def _read_status(self):
-		while True:
-			
-			self.i2c.readfrom_mem_into(MPL3115_I2CADDR, MPL3115_STATUS, self.STA_reg)
+    def _read_status(self):
+        while True:
 
-			if(self.STA_reg[0] == 0):
-				time.sleep(0.01)
-				pass
-			elif(self.STA_reg[0] & 0x04) == 4:
-				return True
-			else:
-				return False
+            self.i2c.readfrom_mem_into(MPL3115_I2CADDR, MPL3115_STATUS, self.STA_reg)
 
-	def _fixed_decimal(self, frac_value):
-		fixed_decimal = 0
+            if(self.STA_reg[0] == 0):
+                time.sleep(0.01)
+                pass
+            elif(self.STA_reg[0] & 0x04) == 4:
+                return True
+            else:
+                return False
 
-		for x in range(2,len(bin(frac_value))):
-			fixed_decimal += int(bin(frac_value)[x])*(2**(-(x-1)))
+    def _fixed_decimal(self, frac_value):
+        fixed_decimal = 0
 
-		return fixed_decimal
+        for x in range(2,len(bin(frac_value))):
+            fixed_decimal += int(bin(frac_value)[x])*(2**(-(x-1)))
 
-	def alt(self):
-		OUT_P_MSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_PRESSURE_DATA_MSB,1)
-		OUT_P_CSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_PRESSURE_DATA_CSB,1)
-		OUT_P_LSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_PRESSURE_DATA_LSB,1)
-		OUT_P_ALL = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_PRESSURE_DATA_MSB,5)
+        return fixed_decimal
 
-		pres_frac = OUT_P_LSB[0] >> 4
-		pres_int = (OUT_P_MSB[0] << 8)|(OUT_P_CSB[0])
+    def alt(self):
+        OUT_P_MSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_PRESSURE_DATA_MSB,1)
+        OUT_P_CSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_PRESSURE_DATA_CSB,1)
+        OUT_P_LSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_PRESSURE_DATA_LSB,1)
+        OUT_P_ALL = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_PRESSURE_DATA_MSB,5)
 
-		if (pres_int & (1 << (16 - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-			pres_int = pres_int - (1 << 16)
+        pres_frac = OUT_P_LSB[0] >> 4
+        pres_int = (OUT_P_MSB[0] << 8)|(OUT_P_CSB[0])
 
-		pres_frac = self._fixed_decimal(pres_frac)
+        if (pres_int & (1 << (16 - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+            pres_int = pres_int - (1 << 16)
 
-		return(float(str(pres_int)+str(pres_frac)[1:]))
+        pres_frac = self._fixed_decimal(pres_frac)
 
-	def temp(self):
-		OUT_T_MSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_TEMP_DATA_MSB,1)
-		OUT_T_LSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_TEMP_DATA_LSB,1)
+        return(float(str(pres_int)+str(pres_frac)[1:]))
 
-		temp_frac = OUT_T_LSB[0] >> 4
+    def temp(self):
+        OUT_T_MSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_TEMP_DATA_MSB,1)
+        OUT_T_LSB = self.i2c.readfrom_mem(MPL3115_I2CADDR, MPL3115_TEMP_DATA_LSB,1)
 
-		temp_int = OUT_T_MSB[0]
+        temp_frac = OUT_T_LSB[0] >> 4
 
-		if (temp_int & (1 << (8 - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-			temp_int = temp_int - (1 << 8)
+        temp_int = OUT_T_MSB[0]
 
-		temp_frac = self._fixed_decimal(temp_frac)
+        if (temp_int & (1 << (8 - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+            temp_int = temp_int - (1 << 8)
 
-		return(float(str(temp_int)+str(temp_frac)[1:]))
+        temp_frac = self._fixed_decimal(temp_frac)
+
+        return(float(str(temp_int)+str(temp_frac)[1:]))
