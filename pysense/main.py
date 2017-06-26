eaa7a5b1e7f1764281f0c8ab5f2eeae5a112d77f
Alex Bucknall <alex@pycom.io>
2017-06-26 13:14:54 +0100

    Updated example Pysense main.py to match updated libraries

--- pysense/main.py
+++ pysense/main.py
@@ -1,3 +1,5 @@
+# See https://docs.pycom.io for more information regarding library specifics
+
 from pysense import Pysense
 from LIS2HH12 import LIS2HH12
 from SI7006A20 import SI7006A20
@@ -5,7 +7,19 @@ from LTR329ALS01 import LTR329ALS01
 from MPL3115A2 import MPL3115A2
 
 py = Pysense()
-pressure = MPL3115A2(py)
-tempHum = SI7006A20(py)
-ambientLight = LTR329ALS01(py)
-acc = LIS2HH12(py)
+mp = MPL3115A2(py,mode=ALTITUDE) # Returns height in meters. Mode may also be set to PRESSURE, returning a value in Pascals
+si = SI7006A20(py)
+lt = LTR329ALS01(py)
+li = LIS2HH12(py)
+
+print(mp.temperature())
+print(mp.altitude())
+print(si.temperature())
+print(si.humidity())
+print(lt.light())
+print(li.acceleration())
+print(li.roll())
+print(li.pitch())
+print(li.yaw())
+
+print(py.read_battery_voltage())
