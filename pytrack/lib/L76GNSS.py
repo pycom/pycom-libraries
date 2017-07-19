1704d61180dc52c38fc7f5214ad8d70eb7236206
Alex Bucknall <alex@pycom.io>
2017-07-19 14:11:19 +0100

    Fixed GPS memory leak by changing Timer.Alarm to Timer.Chrono

--- pytrack/lib/L76GNSS.py
+++ pytrack/lib/L76GNSS.py
@@ -14,12 +14,17 @@ class L76GNSS:
             from machine import I2C
             self.i2c = I2C(0, mode=I2C.MASTER, pins=(sda, scl))
 
+        self.chrono = Timer.Chrono()
+
         self.timeout = timeout
         self.timeout_status = True
-        self.i2c.writeto(GPS_I2CADDR, bytes([0]))
+
+        self.reg = bytearray(1)
+        self.i2c.writeto(GPS_I2CADDR, self.reg)
 
     def _read(self):
-        return self.i2c.readfrom(GPS_I2CADDR, 64)
+        self.reg = self.i2c.readfrom(GPS_I2CADDR, 64)
+        return self.reg
 
     def _convert_coords(self, gngll_s):
         lat = gngll_s[1]
@@ -32,17 +37,20 @@ class L76GNSS:
             lon_d *= -1
         return(lat_d, lon_d)
 
-    def _gps_timeout(self, alarm):
-        alarm.cancel()
-        self.timeout_status = False
-
     def coordinates(self, debug=False):
-        lat_d, lon_d = None, None
+        lat_d, lon_d, debug_timeout = None, None, False
         if self.timeout != None:
-            self._alarm = Timer.Alarm(self._gps_timeout, self.timeout, periodic=False)
+            self.chrono.start()
         nmea = b''
         while True:
+            if self.timeout != None and self.chrono.read() >= self.timeout:
+                self.chrono.stop()
+                chrono_timeout = self.chrono.read()
+                self.chrono.reset()
+                self.timeout_status = False
+                debug_timeout = True
             if self.timeout_status != True:
+                gc.collect()
                 break
             nmea += self._read().lstrip(b'\n\n').rstrip(b'\n\n')
             gngll_idx = nmea.find(b'GNGLL')
@@ -58,15 +66,16 @@ class L76GNSS:
                         pass
                     finally:
                         nmea = nmea[(gngll_idx + e_idx):]
+                        gc.collect()
                         break
             else:
+                gc.collect()
                 if len(nmea) > 4096:
                     nmea = b''
             time.sleep(0.1)
-            gc.collect()
-
         self.timeout_status = True
-        if debug:
-            print('GPS timed out after %d seconds' % (self.timeout))
+        if debug and debug_timeout:
+            print('GPS timed out after %f seconds' % (chrono_timeout))
+            return(None, None)
         else:
             return(lat_d, lon_d)
