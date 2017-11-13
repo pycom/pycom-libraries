8e29ddb4faab25cfa140e77bff1f155f0e1a540a
Catalin Ioana <catalin.ioana@pycom.io>
2017-11-13 11:25:43 +0200

    added INT pin as wakeup reason in pytrack

--- pytrack/lib/pytrack.py
+++ pytrack/lib/pytrack.py
@@ -8,6 +8,7 @@ __version__ = '1.3.0'
 WAKE_REASON_ACCELEROMETER = 1
 WAKE_REASON_PUSH_BUTTON = 2
 WAKE_REASON_TIMER = 4
+WAKE_REASON_INT_PIN = 8
 
 class Pytrack:
 
@@ -42,6 +43,9 @@ class Pytrack:
     IOCAP_ADDR = const(0x391)
     IOCAN_ADDR = const(0x392)
 
+    INTCON_ADDR = const(0x0B)
+    OPTION_REG_ADDR = const(0x95)
+
     _ADCON0_CHS_POSN = const(0x02)
     _ADCON0_ADON_MASK = const(0x01)
     _ADCON1_ADCS_POSN = const(0x04)
@@ -57,7 +61,7 @@ class Pytrack:
 
     WPUA_ADDR = const(0x20C)
 
-    WAKE_REASON_ADDR = const(0x064F)
+    WAKE_REASON_ADDR = const(0x064C)
     MEMORY_BANK_ADDR = const(0x0620)
 
     PCON_ADDR = const(0x096)
@@ -76,6 +80,8 @@ class Pytrack:
         self.clk_cal_factor = 1
         self.reg = bytearray(6)
         self.wake_int = False
+        self.wake_int_pin = False
+        self.wake_int_pin_rising_edge = True
 
         try:
             self.read_fw_version()
@@ -156,7 +162,25 @@ class Pytrack:
         self.magic_write_read(addr, _or=bits)
 
     def get_wake_reason(self):
+        """ returns the wakeup reason, a value out of constants WAKE_REASON_* """
         return self.peek_memory(WAKE_REASON_ADDR)
+
+    def get_sleep_remaining(self):
+        """ returns the remaining time from sleep, as an interrupt (wakeup source) might have triggered """
+        c3 = self.peek_memory(WAKE_REASON_ADDR + 3)
+        c2 = self.peek_memory(WAKE_REASON_ADDR + 2)
+        c1 = self.peek_memory(WAKE_REASON_ADDR + 1)
+        time_device_s = (c3 << 16) + (c2 << 8) + c1
+        # this time is from PIC internal oscilator, so it needs to be adjusted with the calibration value 
+        try:
+            self.calibrate_rtc()
+        except Exception:
+            pass
+        time_s = int((time_device_s / self.clk_cal_factor) + 0.5) # 0.5 used for round
+        # debug code
+        #print("Time on device: "+ str(time_device_s) + " -> " + str(time_s))
+        #time.sleep(0.5)
+        return time_s
 
     def setup_sleep(self, time_s):
         try:
@@ -164,6 +188,8 @@ class Pytrack:
         except Exception:
             pass
         time_s = int((time_s * self.clk_cal_factor) + 0.5)  # round to the nearest integer
+        #print("Time for sleep: " + str(time_s))
+        #time.sleep(0.1)
         self._write(bytes([CMD_SETUP_SLEEP, time_s & 0xFF, (time_s >> 8) & 0xFF, (time_s >> 16) & 0xFF]))
 
     def go_to_sleep(self, gps=True):
@@ -176,15 +202,27 @@ class Pytrack:
         self.poke_memory(ADCON0_ADDR, 0)
 
         if self.wake_int:
-            # Don't touch RA3 or RA5 so that interrupt wake-up works
+            # Don't touch RA3, RA5 or RC1 so that interrupt wake-up works
             self.poke_memory(ANSELA_ADDR, ~((1 << 3) | (1 << 5)))
-            self.poke_memory(ANSELC_ADDR, ~((1 << 6) | (1 << 7)))
+            self.poke_memory(ANSELC_ADDR, ~((1 << 6) | (1 << 7) | (1 << 1)))
         else:
             # disable power to the accelerometer, and don't touch RA3 so that button wake-up works
             self.poke_memory(ANSELA_ADDR, ~(1 << 3))
             self.poke_memory(ANSELC_ADDR, ~(1 << 7))
 
         self.poke_memory(ANSELB_ADDR, 0xFF)
+
+        # check if INT pin (PIC RC1), should be used for wakeup
+        if self.wake_int_pin:
+            if self.wake_int_pin_rising_edge:
+                self.set_bits_in_memory(OPTION_REG_ADDR, 1 << 6) # rising edge of INT pin
+            else:
+                self.mask_bits_in_memory(OPTION_REG_ADDR, ~(1 << 6)) # falling edge of INT pin
+            self.mask_bits_in_memory(ANSELC_ADDR, ~(1 << 1)) # disable analog function for RC1 pin
+            self.set_bits_in_memory(TRISC_ADDR, 1 << 1) # make RC1 input pin
+            self.mask_bits_in_memory(INTCON_ADDR, ~(1 << 1)) # clear INTF
+            self.set_bits_in_memory(INTCON_ADDR, 1 << 4) # enable interrupt; set INTE)
+
         self._write(bytes([CMD_GO_SLEEP]), wait=False)
         # kill the run pin
         Pin('P3', mode=Pin.OUT, value=0)
@@ -230,3 +268,8 @@ class Pytrack:
         else:
             self.mask_bits_in_memory(IOCAN_ADDR, ~(1 << 5))
         self.wake_int = wake_int
+
+    def setup_int_pin_wake_up(self, rising_edge = True):
+        """ allows wakeup to be made by the INT pin (PIC -RC1) """
+        self.wake_int_pin = True
+        self.wake_int_pin_rising_edge = rising_edge
