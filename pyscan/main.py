1cad06d4767b2045ce06b510a68e6d2154ebf51f
Christian Ehlers <christian@ehlers.me.uk>
2018-06-12 21:36:44 +0200

    Add optional debug print statements for simple NFC example

--- pyscan/main.py
+++ pyscan/main.py
@@ -8,8 +8,11 @@ RGB LED is BLUE while waiting for card,
 GREEN if card is valid, RED if card is invalid
 '''
 
+DEBUG = False  # change to True to see debug messages
+
 from pyscan import Pyscan
 from MFRC630 import MFRC630
+import binascii
 import time
 import pycom
 import _thread
@@ -35,21 +38,33 @@ nfc.mfrc630_cmd_init()
 def check_uid(uid, len):
     return VALID_CARDS.count(uid[:len])
 
+def print_debug(msg):
+    if DEBUG:
+        print(msg)
+
 def discovery_loop(nfc, id):
     while True:
         # Send REQA for ISO14443A card type
+        print_debug('Sending REQA for ISO14443A card type...')
         atqa = nfc.mfrc630_iso14443a_WUPA_REQA(nfc.MFRC630_ISO14443_CMD_REQA)
+        print_debug('Response: {}'.format(atqa))
         if (atqa != 0):
             # A card has been detected, read UID
+            print_debug('A card has been detected, read UID...')
             uid = bytearray(10)
             uid_len = nfc.mfrc630_iso14443a_select(uid)
+            print_debug('UID has length: {}'.format(uid_len))
             if (uid_len > 0):
+                print_debug('Checking if card with UID: [{:s}] is listed in VALID_CARDS...'.format(binascii.hexlify(uid[:uid_len],' ').upper()))
                 if (check_uid(list(uid), uid_len)) > 0:
+                    print_debug('Card is listed, turn LED green')
                     pycom.rgbled(RGB_GREEN)
                 else:
+                    print_debug('Card is not listed, turn LED red')
                     pycom.rgbled(RGB_RED)
         else:
             # No card detected
+            print_debug('Did not detect any card...')
             pycom.rgbled(RGB_BLUE)
         nfc.mfrc630_cmd_reset()
         time.sleep(.5)
