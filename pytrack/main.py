93d6c92d3dbf648b7d190551037534eb3cc00db8
Daniel Spindelbauer <spindel.daniel97@gmail.com>
2018-10-03 16:46:03 +0100

    Cleaned up Pytrack example

--- pytrack/main.py
+++ pytrack/main.py
@@ -4,14 +4,11 @@ import network
 import os
 import time
 import utime
+import gc
 from machine import RTC
 from machine import SD
-from machine import Timer
 from L76GNSS import L76GNSS
 from pytrack import Pytrack
-# setup as a station
-
-import gc
 
 time.sleep(2)
 gc.enable()
@@ -23,15 +20,15 @@ utime.sleep_ms(750)
 print('\nRTC Set from NTP to UTC:', rtc.now())
 utime.timezone(7200)
 print('Adjusted from UTC to EST timezone', utime.localtime(), '\n')
+
 py = Pytrack()
 l76 = L76GNSS(py, timeout=30)
-chrono = Timer.Chrono()
-chrono.start()
-#sd = SD()
-#os.mount(sd, '/sd')
-#f = open('/sd/gps-record.txt', 'w')
-while (True):
 
+# sd = SD()
+# os.mount(sd, '/sd')
+# f = open('/sd/gps-record.txt', 'w')
+
+while (True):
     coord = l76.coordinates()
     #f.write("{} - {}\n".format(coord, rtc.now()))
     print("{} - {} - {}".format(coord, rtc.now(), gc.mem_free()))
