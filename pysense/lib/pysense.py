7e980233fa6316b05e35681c9793e0ad4faf3c7f
Catalin Ioana <catalin.ioana@pycom.io>
2020-02-05 19:15:36 +0200

    Pysense improved sleep current (#110)
    * Pysense improved sleep current
    
    pycoproc added constructor parameter
    
    * Pysense - removed unused code
    
    * Pysense, Pytrack, Pyscan library updated init parameters
    
    * Pyscan added copyright

--- pysense/lib/pysense.py
+++ pysense/lib/pysense.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2019, Pycom Limited.
+# Copyright (c) 2020, Pycom Limited.
 #
 # This software is licensed under the GNU GPL version 3 or any
 # later version, with permitted additional terms. For more information
@@ -8,11 +8,13 @@
 # available at https://www.pycom.io/opensource/licensing
 #
 
+# See https://docs.pycom.io for more information regarding library specifics
+
 from pycoproc import Pycoproc
 
-__version__ = '1.4.0'
+__version__ = '1.4.1'
 
 class Pysense(Pycoproc):
 
     def __init__(self, i2c=None, sda='P22', scl='P21'):
-        Pycoproc.__init__(self, i2c, sda, scl)
+        Pycoproc.__init__(self, Pycoproc.PYSENSE, i2c, sda, scl)
