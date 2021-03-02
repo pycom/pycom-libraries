dcb5292283f3e1dd866f04bbc6ef740bd2f5db32
Peter Putz <peter@pycom.io>
2021-03-02 13:27:03 +0100

    pybytes shield fixes

--- shields/pytrack_1_pybytes.py
+++ shields/pytrack_1_pybytes.py
@@ -18,7 +18,7 @@ from machine import RTC
 from machine import SD
 from machine import Timer
 from L76GNSS import L76GNSS
-from pytrack import Pytrack
+from pycoproc_1 import Pycoproc
 from LIS2HH12 import LIS2HH12
 # setup as a station
 
@@ -34,7 +34,7 @@ utime.sleep_ms(750)
 print('\nRTC Set from NTP to UTC:', rtc.now())
 utime.timezone(7200)
 print('Adjusted from UTC to EST timezone', utime.localtime(), '\n')
-py = Pytrack()
+py = Pycoproc(Pycoproc.PYTRACK)
 l76 = L76GNSS(py, timeout=30)
 chrono = Timer.Chrono()
 chrono.start()
@@ -45,7 +45,7 @@ li = LIS2HH12(py)
 while (pybytes):
     coord = l76.coordinates()
     #f.write("{} - {}\n".format(coord, rtc.now()))
-    print('Sending data')
+    print('Sending data', coord)
     pybytes.send_signal(1, coord)
     pybytes.send_signal(2, li.acceleration())
     time.sleep(10)
