d5ce09332ece86c4e87f2f9c6abed6cc0206d357
Michael van Niekerk <mike@agri-io.co.za>
2021-08-07 17:19:46 +0200

    Add region to config in order for nano gateway to start up

--- examples/lorawan-nano-gateway/config.py
+++ examples/lorawan-nano-gateway/config.py
@@ -12,6 +12,7 @@
 
 import machine
 import ubinascii
+from network import LoRa
 
 WIFI_MAC = ubinascii.hexlify(machine.unique_id()).upper()
 # Set  the Gateway ID to be the first 3 bytes of MAC address + 'FFFE' + last 3 bytes of MAC address
@@ -27,6 +28,7 @@ WIFI_SSID = 'my-wifi'
 WIFI_PASS = 'my-wifi-password'
 
 # for EU868
+LORA_REGION = LoRa.EU868
 LORA_FREQUENCY = 868100000
 LORA_GW_DR = "SF7BW125" # DR_5
 LORA_NODE_DR = 5
