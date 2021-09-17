e058410ac0c9c195944d17804149ac4c8c9a5778
peter-pycom <61411318+peter-pycom@users.noreply.github.com>
2021-09-17 14:34:51 +0200

    pybytes

--- shields/shield_2.py
+++ shields/shield_2.py
@@ -157,6 +157,7 @@ else:
     print("button was tapped")
     blink(0x100600) # orange
     print("put pycoproc to sleep and turn pycom module off for", round(sleep_time_s/60,1), "minutes")
+    time.sleep(0.1) # time for print()'s to finish over uart
     pycoproc.setup_sleep(sleep_time_s)
     pycoproc.go_to_sleep()
 
